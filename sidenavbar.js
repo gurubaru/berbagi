ba165d1a910bad4c9b25994369ab351796be9c59
gurubaru <71360199+gurubaru@users.noreply.github.com>
2021-01-26 10:11:15 +0800

    Create sidenavbar.js

---
+++ sidenavbar.js
@@ -0,0 +1,5 @@
+//<![CDATA[
+    function OpenSideNav(){document.getElementById("tutlenc").style.display="block"}
+    function closeSideNav(){document.getElementById("tutlenc").style.display="none"}
+    function showresponddiv(e){var n=e.replace("submenu-","opensubmenu-"),t=document.getElementById(n);current&&current!=t&&(current.style.display="none"),"none"==t.style.display?(t.style.display="block",current=t):t.style.display="none"}var current=null;
+    //]]>
