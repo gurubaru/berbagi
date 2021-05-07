cfb4b56e0b09a07174851bbf2c4cf871d909cb54
gurubaru <71360199+gurubaru@users.noreply.github.com>
2021-05-07 13:51:02 +0800

    Update sidenavbar.js

--- sidenavbar.js
+++ sidenavbar.js
@@ -2,5 +2,4 @@
     function OpenSideNav(){document.getElementById("tutlenc").style.display="block"}
     function closeSideNav(){document.getElementById("tutlenc").style.display="none"}
     function showresponddiv(e){var n=e.replace("submenu-","opensubmenu-"),t=document.getElementById(n);current&&current!=t&&(current.style.display="none"),"none"==t.style.display?(t.style.display="block",current=t):t.style.display="none"}var current=null;
-    function closePesan(){document.getElementById("notifikasi").style.display="none"}
-    //]]>
+//]]>
